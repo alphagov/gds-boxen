166a49c6bfa42511152b5b64abdebd52e24e0d69
Sam J Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2013-02-17 23:59:30 +0000

    Add more repos to the Infrastructure team

--- modules/teams/manifests/infrastructure.pp
+++ modules/teams/manifests/infrastructure.pp
@@ -1,10 +1,15 @@
 class teams::infrastructure {
   include projects::alphagov-deployment
+  include projects::boxes
   include projects::development
   include projects::env-sync-and-backup
   include projects::fabric-scripts
+  include projects::ghtools
+  include projects::packages
   include projects::private-utils
   include projects::puppet
   include projects::smokey
+  include projects::vagrant-govuk
   include projects::vcloud-provisioner
+  include projects::vcloudtools
 }
