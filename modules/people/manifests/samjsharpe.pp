991f0101deff18bee6e13763c26706bf2ed82c00
Sam J Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2013-02-17 19:47:40 +0000

    Add personal config for samjsharpe

---
+++ modules/people/manifests/samjsharpe.pp
@@ -0,0 +1,42 @@
+class people::samjsharpe {
+  include adium
+  include alfred
+  include caffeine
+  include chicken
+  include chrome
+  include colloquy
+  include dropbox
+  include firefox
+  include git-pulls
+  include git-remote-branch
+  include googledrive
+  include iterm2::stable
+  include macvim
+  include mplayerx
+  include onepassword
+  include screen
+  include skype
+  include slate
+  include sparrow
+  include sublime_text_2
+  include turn-off-dashboard
+  include theunarchiver
+  include virtualbox
+  include xquartz
+  include zsh
+
+  # These are all Homebrew packages
+  package {
+    [
+      'autoconf', 'autojump', 'automake', 'bdw-gc', 'boost', 'brew-pip',
+      'cabextract', 'coreutils', 'ctags', 'encfs', 'fdupes', 'fuse4x',
+      'fuse4x-kext', 'gdbm', 'gettext', 'git', 'git-extras', 'git-flow',
+      'git-utils', 'gnupg', 'go', 'jasper', 'jpeg', 'legit', 'libicns',
+      'libpng', 'libpst', 'libtool', 'libyaml', 'nmap', 'ntfs-3g', 'openssl',
+      'parallel', 'pcre', 'pkg-config', 'rdesktop', 'readline', 'rename',
+      'renameutils', 'rlog', 's3cmd', 'sslscan', 'unrar', 'w3m', 'wget', 'xz',
+      'youtube-dl', 'zsh-completions', 'zsh-lovers'
+    ]:
+    ensure => present,
+  }
+}
