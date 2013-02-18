a1ac57becc80116c1ce96503bc8be44e86908c25
Jordan Hatch <jordan@jordanh.net>
2013-02-18 14:26:10 +0000

    Checkout my dotfiles

--- modules/people/manifests/jordanhatch.pp
+++ modules/people/manifests/jordanhatch.pp
@@ -10,6 +10,27 @@ class people::jordanhatch {
 
   include projects::development
 
+  $home = "/Users/${::luser}"
+  $dev = "${home}/dev"
+
+  file { $dev:
+    ensure => directory,
+  }
+
+  $dotfiles = "${dev}/dotfiles"
+
+  repository { $dotfiles:
+    source  => 'JordanHatch/dotfiles',
+    require => File[$dev],
+    notify  => Exec['install-dotfiles'],
+  }
+
+  exec { "install-dotfiles":
+    cwd         => $dotfiles,
+    command     => "make",
+    refreshonly => true
+  }
+
   package { [
    'gdbm',
    'pkg-config',
