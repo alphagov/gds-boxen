21569ba84cc721a033c65cc061c070f1b95bbe3a
Sam J Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2013-02-18 08:36:03 +0000

    git-pulls and git-remote-branch should require ruby

--- modules/git-pulls/manifests/init.pp
+++ modules/git-pulls/manifests/init.pp
@@ -1,14 +1,17 @@
 class git-pulls {
   ruby::gem {'git-pulls-1.8.7':
-    gem  => 'git-pulls',
-    ruby => '1.8.7',
+    gem     => 'git-pulls',
+    ruby    => '1.8.7',
+    require => Class['ruby::1-8-7'],
   }
   ruby::gem {'git-pulls-1.9.2':
-    gem  => 'git-pulls',
-    ruby => '1.9.2',
+    gem     => 'git-pulls',
+    ruby    => '1.9.2',
+    require => Class['ruby::1-9-2'],
   }
   ruby::gem {'git-pulls-1.9.3':
-    gem  => 'git-pulls',
-    ruby => '1.9.3',
+    gem     => 'git-pulls',
+    ruby    => '1.9.3',
+    require => Class['ruby::1-9-3'],
   }
 }
--- modules/git-remote-branch/manifests/init.pp
+++ modules/git-remote-branch/manifests/init.pp
@@ -1,14 +1,17 @@
 class git-remote-branch {
   ruby::gem {'git-remote-branch-1.8.7':
-    gem  => 'grb',
-    ruby => '1.8.7',
+    gem     => 'grb',
+    ruby    => '1.8.7',
+    require => Class['ruby::1-8-7'],
   }
   ruby::gem {'git-remote-branch-1.9.2':
-    gem  => 'grb',
-    ruby => '1.9.2',
+    gem     => 'grb',
+    ruby    => '1.9.2',
+    require => Class['ruby::1-9-2'],
   }
   ruby::gem {'git-remote-branch-1.9.3':
-    gem  => 'grb',
-    ruby => '1.9.3',
+    gem     => 'grb',
+    ruby    => '1.9.3',
+    require => Class['ruby::1-9-3'],
   }
 }
