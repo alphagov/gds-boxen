3f8e3171d52cc8180ad50301d3840b21d7efd098
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2013-02-19 12:43:05 +0000

    [bradleywright] More changes, including default Emacs

--- modules/people/manifests/bradleywright.pp
+++ modules/people/manifests/bradleywright.pp
@@ -25,24 +25,31 @@ class people::bradleywright {
   include projects::whitehall
 
   $home     = "/Users/${::luser}"
-  $projects = "${home}/Projects"
+  $home_projects = "${home}/Projects"
 
-  file { $projects:
+  file { $home_projects:
     ensure => directory,
   }
 
-  $dotfiles = "${projects}/dotfiles"
+  # Emacs overrides for Rails projects
+  file { "${boxen::config::srcdir}/.dir-locals.el":
+    content => "((js2-mode . ((js2-basic-offset . 2)))
+ (scss-mode . ((css-indent-offset . 2))))",
+    require => File["${boxen::config::srcdir}"],
+  }
+
+  $dotfiles = "${home_projects}/dotfiles"
 
   repository { $dotfiles:
     source  => 'bradleywright/dotfiles',
-    require => File[$projects]
+    require => File[$home_projects]
   }
 
-  $emacs = "${projects}/emacs-d"
+  $emacs = "${home_projects}/emacs-d"
 
   repository { $emacs:
     source  => 'bradleywright/emacs-d',
-    require => File[$projects]
+    require => File[$home_projects]
   }
 
   file { "${home}/.local_zshrc":
@@ -51,12 +58,12 @@ class people::bradleywright {
 
 export govuk_dev_dist='lucid'
 
-alias vup=\"cd ~/src/puppet; git pull; cd ~/src/development; git pull;vagrant destroy; govuk_dev_dist=lucid vagrant up; ssh dev govuk_puppet; vagrant provision; ssh dev 'cd ~/src/dotfiles && make'; ssh dev\"",
+alias vup=\"cd ~/src/puppet; git pull; cd ~/src/development; git pull;vagrant destroy; govuk_dev_dist=lucid vagrant up; ssh dev 'sudo apt-get -y install libxml2-dev libxslt-dev && govuk_puppet'; vagrant provision; ssh dev 'cd ~/src/dotfiles && make'; ssh dev\"",
   }
 
   file { "${home}/.localgitconfig":
-    content => '[user]
     mode    => '0644',
+    content => '[user]
     email = bradley.wright@digital.cabinet-office.gov.uk
 [credential]
     helper = osxkeychain',
