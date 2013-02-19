c98fcdfd5b16539571a63afe290c6fdf9a72ffc7
Sam J Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2013-02-19 10:48:46 +0000

    Add some gem pre-reqs to vagrant-dns

--- modules/vagrant-dns/manifests/init.pp
+++ modules/vagrant-dns/manifests/init.pp
@@ -17,4 +17,42 @@ class vagrant-dns {
     require => Class['ruby::1-9-3'],
     version => '0.2.4',
   }
+  # vagrant-dns has a tacit dependency on daemons: 
+  #   https://github.com/BerlinVagrant/vagrant-dns/issues/7
+  ruby::gem {'daemons-1.8.7':
+    gem     => 'daemons',
+    ruby    => '1.8.7',
+    require => Class['ruby::1-8-7'],
+    version => '1.1.8',
+  }
+  ruby::gem {'daemons-1.9.2':
+    gem     => 'daemons',
+    ruby    => '1.9.2',
+    require => Class['ruby::1-9-2'],
+    version => '1.1.8',
+  }
+  ruby::gem {'daemons-1.9.3':
+    gem     => 'daemons',
+    ruby    => '1.9.3',
+    require => Class['ruby::1-9-3'],
+    version => '1.1.8',
+  }
+  ruby::gem {'rubydns-1.8.7':
+    gem     => 'rubydns',
+    ruby    => '1.8.7',
+    require => Class['ruby::1-8-7'],
+    version => '0.4.1',
+  }
+  ruby::gem {'rubydns-1.9.2':
+    gem     => 'rubydns',
+    ruby    => '1.9.2',
+    require => Class['ruby::1-9-2'],
+    version => '0.4.1',
+  }
+  ruby::gem {'rubydns-1.9.3':
+    gem     => 'rubydns',
+    ruby    => '1.9.3',
+    require => Class['ruby::1-9-3'],
+    version => '0.4.1',
+  }
 }
