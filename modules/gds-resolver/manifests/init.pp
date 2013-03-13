ccc84e35f318c5038350f75f7b42a9071abac235
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2013-03-13 21:46:43 +0000

    Add gds-resolver class
    This allows us to hit *.gds more easily.

---
+++ modules/gds-resolver/manifests/init.pp
@@ -0,0 +1,11 @@
+# Closed: Installs resolv.conf entry for *.gds
+#
+# Example
+#
+#   include gds-resolver
+
+class gds-resolver {
+  osx_resolver::domain { 'gds':
+    content => 'nameserver 192.168.19.254',
+  }
+}
