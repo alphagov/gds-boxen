6012881a0b08889f643b44d45472ac0aa0dd08dc
James Abley <james.abley@gmail.com>
2013-03-14 17:43:24 +0000

    Remove homebrew packages
    Make the initial install faster so that I can iterate
    and fix what's broken.

--- modules/people/manifests/jabley.pp
+++ modules/people/manifests/jabley.pp
@@ -17,66 +17,6 @@ class people::jabley {
   # Projects only accessible to certain staff
   include projects::deployment
 
-  # These are all Homebrew packages
-  package {
-    [
-      'ascii',
-      'bash-completion',
-      'boost',
-      'bsdmake',
-      'cloc',
-      'cmake',
-      'dos2unix',
-      'ec2-api-tools',
-      'emacs',
-      'faac',
-      'ffmpeg',
-      'gettext',
-      'git',
-      'glew',
-      'glm',
-      'gmp',
-      'gnu-typist',
-      'groovy',
-      'httperf',
-      'id3tool',
-      'jpeg',
-      'lame',
-      'lynx',
-      'maven',
-      'md5sha1sum',
-      'mobile-shell',
-      'mysql',
-      'nmap',
-      'node',
-      'oniguruma',
-      'p7zip',
-      'parallel',
-      'pcre',
-      'pidof',
-      'pigz',
-      'pkg-config',
-      'pngcrush',
-      'proctools',
-      'protobuf',
-      'pv',
-      'redis',
-      'sbt',
-      'scala',
-      'sdl',
-      'sdl_image',
-      'sloccount',
-      'ssh-copy-id',
-      'tmux',
-      'wget',
-      'wrk',
-      'x264',
-      'xvid',
-      'yasm',
-    ]:
-    ensure => 'present',
-  }
-
   $home = "/Users/${::luser}"
   $projects = "${home}/Projects"
 
