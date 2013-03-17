fe0c87cf4509880376147b056a37625b2813afd4
James Abley <james.abley@gmail.com>
2013-03-17 20:28:26 +0000

    Add performance platform repos
    Use repo directly rather than create project definitions since
    I think people will want include the team definition rather than
    individual repos. If that's not the case, we can revisit this.

--- modules/teams/manifests/performance-platform.pp
+++ modules/teams/manifests/performance-platform.pp
@@ -5,10 +5,25 @@ class teams::performance-platform {
   include projects::alphagov-deployment
   include projects::puppet
 
-  # Frontend
-  #include projects::datainsight-frontend
-  #include projects::limelight
-
-  # Backend
-  #include projects::backstory
+  repo::gds { 'backdrop': }
+  repo::alphagov { 'datainsight': }
+  repo::alphagov { 'datainsight-akamai-scanner': }
+  repo::alphagov { 'datainsight-collectors': }
+  repo::alphagov { 'datainsight-everything-recorder': }
+  repo::alphagov { 'datainsight-format-success-recorder': }
+  repo::alphagov { 'datainsight-frontend': }
+  repo::alphagov { 'datainsight-ga-collector': }
+  repo::alphagov { 'datainsight-ga-functional-test': }
+  repo::alphagov { 'datainsight-govuk-collector': }
+  repo::alphagov { 'datainsight-insidegov-collector': }
+  repo::alphagov { 'datainsight-insidegov-recorder': }
+  repo::alphagov { 'datainsight-narrative-recorder': }
+  repo::alphagov { 'datainsight-nongovuk-reach-collector': }
+  repo::alphagov { 'datainsight-release-info': }
+  repo::alphagov { 'datainsight-todays-activity-recorder': }
+  repo::alphagov { 'datainsight-weekly-reach-recorder': }
+  repo::alphagov { 'datainsight_collector': }
+  repo::alphagov { 'datainsight_logging': }
+  repo::alphagov { 'datainsight_recorder': }
+  repo::alphagov { 'limelight': }
 }
