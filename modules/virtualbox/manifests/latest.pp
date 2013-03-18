72b2ae6ac8222cbc5fae20442947dc23bdc2bcb6
Sam J Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2013-03-18 00:07:14 +0000

    Fix errors in the virtualbox module

--- modules/virtualbox/manifests/latest.pp
+++ modules/virtualbox/manifests/latest.pp
@@ -1,5 +1,5 @@
-class virtualbox::latest {}
-  package { 'VirtualBox':
+class virtualbox::latest {
+  package { 'VirtualBox-latest':
     provider => 'pkgdmg',
     source   => 'http://download.virtualbox.org/virtualbox/4.2.10/VirtualBox-4.2.10-84104-OSX.dmg',
   }
