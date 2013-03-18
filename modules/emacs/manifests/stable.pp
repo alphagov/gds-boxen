00fd52e9589b5c8ed35451bb7bbf45af0d0fba10
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2013-03-18 11:17:26 +0000

    Latest Emacs; fix emacs termcap

--- modules/emacs/manifests/stable.pp
+++ modules/emacs/manifests/stable.pp
@@ -7,6 +7,13 @@
 class emacs::stable {
   package { 'Emacs':
     provider => 'appdmg',
-    source   => 'http://emacsformacosx.com/emacs-builds/Emacs-24.2-universal-10.6.8.dmg'
+    source   => 'http://emacsformacosx.com/emacs-builds/Emacs-24.3-universal-10.6.8.dmg',
+    notify   => 'fix-emacs-termcap',
+  }
+
+  # So ansi-term behaves itself
+  exec { 'fix-emacs-termcap':
+    command     => 'tic -o ~/.terminfo /Applications/Emacs.app/Contents/Resources/etc/e/eterm-color.ti',
+    refreshonly => true,
   }
 }
