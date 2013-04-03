5189d879a659d8292782e711711589998246df32
Mark Norman Francis <mark.norman.francis@digital.cabinet-office.gov.uk>
2013-04-03 11:09:07 +0100

    [norm] Create personal manifest for norm.

---
+++ modules/people/manifests/norm.pp
@@ -0,0 +1,19 @@
+class people::norm {
+  include chrome
+  include dropbox
+  include macvim
+  include onepassword
+  include textmate
+  include things
+  include wget
+
+  include teams::performance-platform
+
+  package {
+    [
+      'bash-completion',
+      'redis',
+    ]:
+    ensure => present,
+  }
+}
