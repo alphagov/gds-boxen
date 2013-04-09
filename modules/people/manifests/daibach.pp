a4d5483414c56b732498d0b9470473ce1712beed
Dafydd Vaughan <dafydd.vaughan@digital.cabinet-office.gov.uk>
2013-04-09 14:23:19 +0100

    add daibach initial boxen config

---
+++ modules/people/manifests/daibach.pp
@@ -0,0 +1,25 @@
+class people::daibach {
+
+  include alfred::two
+  include caffeine
+  include chrome
+  include dropbox
+  include gitx
+  include iterm2
+  include mysql
+  include onepassword
+  include openconnect
+  include propane
+  include remove-spotlight
+  include spotify
+  include textmate
+  include turn-off-dashboard
+  include vagrant
+  include vagrant-dns
+  include virtualbox
+  include wget
+
+  include teams::mainstream
+
+
+}
