54a16877b22a4e392f284c771bee7ac82dc6528b
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2013-04-29 09:44:37 +0100

    turn-off-dashboard now uses osx::defaults

--- modules/turn-off-dashboard/manifests/init.pp
+++ modules/turn-off-dashboard/manifests/init.pp
@@ -1,13 +1,12 @@
 # Turns off the OSX Dashboard feature
 class turn-off-dashboard {
-  exec { 'disable-dashboard':
-    command => 'defaults write com.apple.dashboard mcx-disabled -boolean YES',
-    unless  => '/usr/bin/defaults read com.apple.dashboard mcx-disabled | grep -Fqe 1',
-    notify  => Exec['dashboard-kill-dock'],
-  }
-  exec { 'dashboard-kill-dock':
-    command     => 'killall Dock',
-    user        => $user,
-    refreshonly => true,
+  include osx::dock
+
+  boxen::osx_defaults { 'Disables Dashboard':
+    user   => $::boxen_user,
+    domain => 'com.apple.dashboard',
+    key    => 'mcx-disabled',
+    value  => true,
+    notify => Exec['killall Dock'];
   }
 }
