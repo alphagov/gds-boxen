438ffdf56525311772ebbc8ad24977a4ee9dcbf7
Sam J Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2013-04-30 12:37:46 +0100

    [carlmassa] Add a config for Carl

---
+++ modules/people/manifests/carlmassa.pp
@@ -0,0 +1,67 @@
+class people::carlmassa {
+  include chrome
+  include dropbox
+  include firefox
+  include gds-resolver
+  include github_for_mac
+  include gitx::dev
+  include gnupg
+  include googledrive
+  include hub
+  include iterm2::stable
+  include mou
+  include mplayerx
+  include openconnect
+  include screen
+  include unarchiver
+  include vagrant_gem
+  include vagrant-dns
+  include vagrant-vbguest
+  include virtualbox::latest
+
+  # Projects accessible to everyone in Infrastructure
+  include teams::infrastructure
+  # Projects only accessible to certain staff
+  include projects::deployment
+  include projects::deployment::creds
+
+  file {"${boxen::config::srcdir}/development/Vagrantfile.local":
+    content => "
+        config.vm.network :hostonly, '10.23.45.67'
+        config.vm.customize ['modifyvm', :id, '--memory', 1024]
+        config.vm.customize ['modifyvm', :id, '--cpus', 2]
+        config.vm.customize ['modifyvm', :id, '--name', 'lucid_dev_vm']
+",
+    require => Class['Projects::Development']
+  }
+
+  # These are all Homebrew packages
+  package {
+    [
+      'brew-pip',
+      'brew-gem',
+      'htop-osx',
+      'nmap',
+      'ntfs-3g',
+      'osxutils',
+      'parallel',
+      's3cmd',
+      'ssh-copy-id',
+      'unrar',
+      'wget',
+    ]:
+    ensure => present,
+  }
+
+  # Settings from puppet-osx
+  include osx::disable_app_quarantine
+  include osx::dock::2d
+  include osx::finder::empty_trash_securely
+  include osx::finder::show_all_on_desktop
+  include osx::finder::unhide_library
+  include osx::global::expand_print_dialog
+  include osx::global::expand_save_dialog
+  include osx::no_network_dsstores
+  osx::recovery_message { 'If found, please call +44 (0) 7447 907 231': }
+
+}
