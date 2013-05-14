fb087db5923cd173f89a6b6f9fc6e0e2a486b200
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2013-05-14 09:03:15 +0100

    [robyoung] Add personal config for robyoung

---
+++ modules/people/manifests/robyoung.pp
@@ -0,0 +1,56 @@
+class people::robyoung {
+  include caffeine
+  include chrome
+  include dropbox
+  include firefox
+  include gds-resolver
+  include git
+  include iterm2::stable
+  include ohmyzsh
+  include openconnect
+  include propane
+  include pycharm
+  include rubymine
+  include sublime_text_2
+  include virtualbox::latest
+  include zsh
+
+  include teams::performance-platform
+  include projects::deployment
+  include projects::vcloud-templates
+
+  $home = "/Users/${::luser}"
+  $projects = "${home}/Projects"
+
+  file { $projects:
+    ensure => directory,
+  }
+
+  # Homebrew packages
+  package {
+    [
+      'bash-completion',
+      'htop-osx',
+      'go',
+      'mongodb',
+      'mysql',
+      'python',
+      'redis',
+      'zeromq',
+      'zsh-completions',
+      'zsh-lovers',
+      'wget',
+    ]:
+    ensure => present,
+  }
+
+  # Python packages
+  package {
+    [
+      'virtualenv',
+      'virtualenvwrapper',
+    ]:
+    ensure => present,
+    provider => pip,
+  }
+}
