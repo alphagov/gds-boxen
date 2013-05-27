d9d7369df4fbaa4b6c539eb1992dadc97d0e7632
Sam J Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2013-05-27 16:19:07 +0100

    [samjsharpe] Add my vagrant-rackspace repo

--- modules/people/manifests/samjsharpe/personal_repos.pp
+++ modules/people/manifests/samjsharpe/personal_repos.pp
@@ -3,6 +3,9 @@ class people::samjsharpe::personal_repos ($project_home){
   repository { "${project_home}/website":
     source => 'samjsharpe/samjsharpe.github.com'
   }
+  repository { "${project_home}/rackspace":
+    source => 'samjsharpe/rackspace'
+  }
   repository { "${project_home}/dotfiles":
     source  => 'samjsharpe/dotfiles',
     notify  => Exec['samjsharpe-link-my-dotfiles'],
