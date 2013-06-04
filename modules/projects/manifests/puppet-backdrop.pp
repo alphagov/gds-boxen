9918fed123508efbee1c3dc9d2e4107376e64f1c
Mark Norman Francis <mark.norman.francis@digital.cabinet-office.gov.uk>
2013-06-04 11:19:17 +0100

    Include puppet-backdrop for performance platform.

---
+++ modules/projects/manifests/puppet-backdrop.pp
@@ -0,0 +1,4 @@
+# Pulls the https://github.com/alphagov/puppet-backdrop repository
+class projects::puppet-backdrop {
+  repo::alphagov { 'puppet-backdrop': }
+}
--- modules/teams/manifests/performance-platform/puppet.pp
+++ modules/teams/manifests/performance-platform/puppet.pp
@@ -1,4 +1,5 @@
 class teams::performance-platform::puppet {
   include projects::pp-puppet-secrets
   include projects::pp-puppet
+  include projects::puppet-backdrop
 }
