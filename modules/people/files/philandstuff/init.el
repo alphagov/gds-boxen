f20551660f7435ba050482fb6f9d17db196d2be3
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2013-07-17 10:43:42 +0100

    [philandstuff] disable emacs ui frills

--- modules/people/files/philandstuff/init.el
+++ modules/people/files/philandstuff/init.el
@@ -3,3 +3,10 @@
     '("marmalade" .
       "http://marmalade-repo.org/packages/"))
 (package-initialize)
+
+;; disable ui frills
+(dolist (mode '(menu-bar-mode tool-bar-mode scroll-bar-mode))
+  (when (fboundp mode) (funcall mode -1)))
+
+
+;;TODO: install packages
