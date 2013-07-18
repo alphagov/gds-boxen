89b58c12bbafcf5e76610f32dd65bb7753aa4b7d
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2013-07-18 17:12:51 +0100

    [philandstuff] more emacs config stuff

--- modules/people/files/philandstuff/init.el
+++ modules/people/files/philandstuff/init.el
@@ -8,8 +8,17 @@
 (dolist (mode '(menu-bar-mode tool-bar-mode scroll-bar-mode))
   (when (fboundp mode) (funcall mode -1)))
 
+;; better unique buffer names
+(require 'uniquify)
+
+;; enable ido
+(require 'ido)
+(ido-mode t)
+
+(setq inhibit-startup-message t)
+
 (defconst important-packages
-  '(magit org paredit puppet-mode)
+  '(magit org paredit puppet-mode smex)
   "packages to ensure are always present on startup")
 
 (require 'cl-lib)
@@ -18,3 +27,10 @@
   (dolist (pkg important-packages)
     (when (not (package-installed-p pkg))
       (package-install pkg))))
+
+;; enable ido for M-x
+(setq smex-save-file (concat user-emacs-directory ".smex-items"))
+(smex-initialize)
+(global-set-key (kbd "M-x") 'smex)
+
+
