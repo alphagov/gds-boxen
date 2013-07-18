b52e2eb27ca5cb8ae529e634ee4d07d5edbfc6c0
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2013-07-18 16:17:58 +0100

    [philandstuff] don't refresh emacs packages on every startup

--- modules/people/files/philandstuff/init.el
+++ modules/people/files/philandstuff/init.el
@@ -8,14 +8,13 @@
 (dolist (mode '(menu-bar-mode tool-bar-mode scroll-bar-mode))
   (when (fboundp mode) (funcall mode -1)))
 
-
-;;TODO: don't refresh packages on every startup
-(package-refresh-contents)
-
 (defconst important-packages
-  '(magit paredit puppet-mode)
+  '(magit org paredit puppet-mode)
   "packages to ensure are always present on startup")
 
-(dolist (pkg important-packages)
-  (when (not (package-installed-p pkg))
-    (package-install pkg)))
+(require 'cl-lib)
+(when (cl-notevery 'package-installed-p important-packages)
+  (package-refresh-contents)
+  (dolist (pkg important-packages)
+    (when (not (package-installed-p pkg))
+      (package-install pkg))))
