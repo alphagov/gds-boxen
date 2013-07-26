17657bb44ab4cd635008f8754d80c309ba18db66
Ralph Cowling <acastofthousands@gmail.com>
2013-07-26 12:13:15 +0100

    get rid of sublime text 3 module until it’s ready

--- modules/people/manifests/roc.pp
+++ modules/people/manifests/roc.pp
@@ -2,12 +2,14 @@ class people::roc {
   include chrome
   include gds-resolver
   include git
-  include vagrant
+  # include vagrant
+  include vagrant_gem
+  include vagrant-dns
   include virtualbox
   include wget  
   include dropbox
   include firefox
-  include sublime_text_3
+  # include sublime_text_3
 
   # Team repos
   include teams::performance-platform
