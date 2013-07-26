d2a702a453ad42edfbc99da1d5cb366c973ffc1e
Ralph Cowling <acastofthousands@gmail.com>
2013-07-26 12:00:03 +0100

    adding roc.pp into the mix

---
+++ modules/people/manifests/roc.pp
@@ -0,0 +1,27 @@
+class people::roc {
+  include chrome
+  include gds-resolver
+  include git
+  include vagrant
+  include virtualbox
+  include wget  
+  include dropbox
+  include firefox
+  include sublime_text_3
+
+  # Team repos
+  include teams::performance-platform
+  repo::alphagov { 'government-service-design-manual': }
+
+
+  # Homebrew packages
+  package {
+    [
+      'bash-completion',
+      'redis'
+    ]:
+    ensure => present,
+  }
+
+
+}
