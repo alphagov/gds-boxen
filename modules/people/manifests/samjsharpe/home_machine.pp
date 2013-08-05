872d367d3ceb5853aa688d6b813e8b997a164f5d
Sam J Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2013-08-06 00:36:25 +0100

    [samjsharpe] Add vagrant_no_vbox module to home machine

--- modules/people/manifests/samjsharpe/home_machine.pp
+++ modules/people/manifests/samjsharpe/home_machine.pp
@@ -8,5 +8,6 @@ class people::samjsharpe::home_machine {
       project_home => $personal_projects,
     }
     include chicken
+    include vagrant_no_vbox
     osx::recovery_message { 'If found, please call +44 (0) 7712 046 007': }
 }
