16cf8ab4c7c1facdc46ee8c45d82d9fe0afcfd1c
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2013-08-12 15:23:00 +0100

    gds-development: Ensure that old vagrant gems die
    If any of these gems exist they'll block the correct version of Vagrant
    from operating when running `vagrant up`.
    
    There's probably a more elegant way to do this, but this appears to work.

--- modules/gds-development/manifests/init.pp
+++ modules/gds-development/manifests/init.pp
@@ -15,4 +15,23 @@ class gds-development {
   # Dev uses the vagrant-dns plugin
   vagrant::plugin { 'vagrant-dns': }
 
+  # Make sure we remove the old gems
+  ruby::gem {"vagrant-1.8.7":
+    gem     => 'vagrant',
+    ruby    => '1.8.7',
+    require => Class['ruby::1_8_7'],
+    ensure  => absent,
+  }
+  ruby::gem {"vagrant-1.9.2":
+    gem     => 'vagrant',
+    ruby    => '1.9.2',
+    require => Class['ruby::1_9_2'],
+    ensure  => absent,
+  }
+  ruby::gem {"vagrant-1.9.3":
+    gem     => 'vagrant',
+    ruby    => '1.9.3',
+    require => Class['ruby::1_9_3'],
+    ensure  => absent,
+  }
 }
