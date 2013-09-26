b9f75064a36e537a94984a319053586373ec1fff
Jenny Duckett <jenny.duckett@digital.cabinet-office.gov.uk>
2013-09-26 10:37:40 +0100

    [jennyd] Add projects, vagrant plugins, settings, dotfiles

--- modules/people/manifests/jennyd.pp
+++ modules/people/manifests/jennyd.pp
@@ -1,13 +1,24 @@
 class people::jennyd {
   include chrome
+  include dropbox
   include encfs
   include gds-development
   include gnupg
   include iterm2::stable
-  include postgresql
+  include skype
   include sublime_text_2
   include turn-off-dashboard
   include wget
+
+  vagrant::plugin { 'vagrant-cachier': }
+  vagrant::plugin { 'vagrant-zz-multiprovider-snap': }
+
+  include projects::development
+  include projects::fabric-scripts
+  include projects::opsmanual
+  include projects::puppet
+  include projects::redirector
+  include projects::vagrant-govuk
 
   Boxen::Osx_defaults {
     user => $::luser,
@@ -17,6 +28,17 @@ class people::jennyd {
     key    => 'DSDontWriteNetworkStores',
     domain => 'com.apple.dashboard',
     value  => 'true',
+  }
+
+  include osx::dock::autohide
+  include osx::disable_app_quarantine
+
+  class { 'osx::global::key_repeat_delay':
+    delay => 200
+  }
+
+  class { 'osx::global::key_repeat_rate':
+    rate => 400
   }
 
   package {
@@ -37,6 +59,18 @@ class people::jennyd {
     provider => pip,
   }
 
-  include osx::dock::autohide
-  include osx::disable_app_quarantine
+  $home     = "/Users/${::luser}"
+  $dotfiles = "${home}/dotfiles"
+
+  repository { $dotfiles:
+    source  => 'jennyd/dotfiles',
+    notify  => Exec['join-the-dots'],
+  }
+
+  exec { 'join-the-dots':
+    cwd         => $dotfiles,
+    command     => "./join-the-dots.sh",
+    refreshonly => true,
+  }
+
 }
