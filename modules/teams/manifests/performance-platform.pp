784fab15e2c4ed5fd7774b83e4f294e4e7a856ac
James Abley <james.abley@gmail.com>
2013-10-11 14:18:14 +0100

    Add pp-manual to pp team profile

--- modules/teams/manifests/performance-platform.pp
+++ modules/teams/manifests/performance-platform.pp
@@ -36,6 +36,7 @@ class teams::performance-platform {
 
   # New World Order
   repo::gds      { 'google-dev-credentials': }
+  repo::gds      { 'pp-manual': }
   repo::gds      { 'pp-sync': }
   repo::alphagov { 'backdrop': }
   repo::alphagov { 'backdropsend': }
