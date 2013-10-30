696008a1bd50af710671ed7b6d1cfb55f1a033d1
russell thorn <russell.thorn@digital.cabinet-office.gov.uk>
2013-10-30 16:31:00 +0000

    Add a personal boxen config

---
+++ modules/people/manifests/russellthorn.pp
@@ -0,0 +1,11 @@
+class people::russellthorn {
+  include gds-resolver
+
+  include projects::development
+  include projects::frontend
+  include projects::govuk_frontend_toolkit
+  include projects::static
+  include projects::whitehall
+
+  include gds-development
+}
