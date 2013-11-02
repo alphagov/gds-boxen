e8be58176683c14615b7608ba0687acefd86187c
Sam J Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2013-11-02 17:44:42 +0000

    [samjsharpe] populate some HMRC repos

--- modules/people/manifests/samjsharpe/hmrc_repos.pp
+++ modules/people/manifests/samjsharpe/hmrc_repos.pp
@@ -5,4 +5,40 @@ class people::samjsharpe::hmrc_repos ($project_home){
     source  => 'alphagov/machine-bootstrap',
     require      => File[$project_home],
   }
+  repository { "${project_home}/jenkins-config":
+    source  => "git@github.gds:HMRC/jenkins-config",
+    require => File[$project_home],
+  }
+  repository { "${project_home}/vcloud-config":
+    source  => "git@github.gds:HMRC/vcloud-config",
+    require => File[$project_home],
+  }
+  repository { "${project_home}/hmrc-puppet":
+    source  => "git@github.gds:HMRC/puppet",
+    require => File[$project_home],
+  }
+  repository { "${project_home}/vcb":
+    source  => "git@github.gds:HMRC/vcb",
+    require => File[$project_home],
+  }
+  repository { "${project_home}/puppet-secrets":
+    source  => "git@github.gds:HMRC/puppet-secrets",
+    require => File[$project_home],
+  }
+  repository { "${project_home}/deployment":
+    source  => "git@github.gds:HMRC/deployment",
+    require => File[$project_home],
+  }
+  repository { "${project_home}/opsmanual":
+    source  => "git@github.gds:HMRC/opsmanual",
+    require => File[$project_home],
+  }
+  repository { "${project_home}/local-dev-vm":
+    source  => "git@github.gds:HMRC/local-dev-vm",
+    require => File[$project_home],
+  }
+  repository { "${project_home}/puppet-test":
+    source  => "git@github.gds:HMRC/puppet-test",
+    require => File[$project_home],
+  }
 }
