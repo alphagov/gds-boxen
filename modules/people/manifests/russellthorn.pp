2782eda67e6b8c3c998a663179936f733ce59251
russell thorn <russell.thorn@digital.cabinet-office.gov.uk>
2013-11-08 14:51:07 +0000

    Russels config

--- modules/people/manifests/russellthorn.pp
+++ modules/people/manifests/russellthorn.pp
@@ -1,11 +1,17 @@
 class people::russellthorn {
+  $vagrant_ip = '10.1.1.254'
+
+  include gds-development
   include gds-resolver
+  
+  class { 'gds-ssh-config': }
+  ssh_config::fragment{'user':
+    content => template('people/russellthorn/ssh_config'),
+  }
 
   include projects::development
   include projects::frontend
   include projects::govuk_frontend_toolkit
   include projects::static
   include projects::whitehall
-
-  include gds-development
 }
