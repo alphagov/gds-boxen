557eaf5fa261c484996b4e31a479d60e41c8fb8d
bob walker <bob@randomness.org.uk>
2013-11-18 23:57:05 +0000

    Configure a mac machine the way I like. possibly.

---
+++ modules/people/manifests/rjw1.pp
@@ -0,0 +1,53 @@
+class people::rjw1 {
+  include chrome
+  include firefox
+  include encfs
+  include gds_vpn_profiles
+  include git
+  include gnupg
+  include iterm2::stable
+  include vagrant
+  include virtualbox
+
+  include teams::infrastructure
+
+  include projects::deployment::creds
+
+
+
+  repository { "/Users/bobwalker/git/dotfiles":
+    source  => 'git@github.com:rjw1/dotfiles.git',
+  }
+
+  repository { "/Users/bobwalker/git/OpenGuides":
+    source  => 'git@github.com:OpenGuides/OpenGuides.git',
+  }
+
+  boxen::osx_defaults { "Disable 'natural scrolling'":
+    key    => 'com.apple.swipescrolldirection',
+    domain => 'NSGlobalDomain',
+    value  => 'false',
+    type   => 'bool',
+  }
+
+  boxen::osx_defaults { 'Do not create .DS_Store':
+    key    => 'DSDontWriteNetworkStores',
+    domain => 'com.apple.dashboard',
+    value  => 'true',
+  }
+
+package {
+    [
+      'python',
+      'pwgen',
+      'tmux',
+      'bash-completion',
+      'vim',
+    ]:
+    ensure => present,
+  }
+
+  vim::bundle { 'rodjek/vim-puppet': }
+  vim::bundle { 'godlygeek/tabular': }
+}
+
