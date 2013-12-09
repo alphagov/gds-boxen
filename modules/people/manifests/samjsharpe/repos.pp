ba1186e689edbdbb8cddf88c380d9e738b969a1f
Sam J Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2013-12-09 11:14:18 +0000

    [samjsharpe] refactor slightly

--- modules/people/manifests/samjsharpe/repos.pp
+++ modules/people/manifests/samjsharpe/repos.pp
@@ -14,36 +14,47 @@ class people::samjsharpe::repos {
     # HMRC repos
     people::samjsharpe::repos::gh  {'hmrc/base-image-builder':}
 
-    people::samjsharpe::repos::gds {'hmrc/deployment':}
-    people::samjsharpe::repos::gds {'hmrc/firewall-rule-generator':}
-    people::samjsharpe::repos::gds {'hmrc/local-dev-vm':}
-    people::samjsharpe::repos::gds {'hmrc/opsmanual':}
-    people::samjsharpe::repos::gds {'hmrc/puppet':}
-    people::samjsharpe::repos::gds {'hmrc/puppet-secrets':}
-    people::samjsharpe::repos::gds {'hmrc/puppet-test':}
-    people::samjsharpe::repos::gds {'hmrc/vcb':}
-    people::samjsharpe::repos::gds {'hmrc/vcloud-config':}
+    people::samjsharpe::repos::gds {[
+        'hmrc/deployment',
+        'hmrc/firewall-rule-generator',
+        'hmrc/local-dev-vm',
+        'hmrc/opsmanual',
+        'hmrc/puppet',
+        'hmrc/puppet-secrets',
+        'hmrc/puppet-test',
+        'hmrc/vcb',
+        'hmrc/vcloud-config'
+        ]:}
 
-    people::samjsharpe::repos::gds {'gds/deployment':}
-    people::samjsharpe::repos::gds {'gds/development':}
-    people::samjsharpe::repos::gds {'gds/env-sync-and-backup':}
-    people::samjsharpe::repos::gds {'gds/opsmanual':}
-    people::samjsharpe::repos::gds {'gds/private-utils':}
-    people::samjsharpe::repos::gds {'gds/puppet':}
+    # GDS Repos
+    people::samjsharpe::repos::gds {[
+        'gds/deployment',
+        'gds/development',
+        'gds/env-sync-and-backup',
+        'gds/opsmanual',
+        'gds/private-utils',
+        'gds/puppet'
+        ]:}
 
-    people::samjsharpe::repos::gh  {'alphagov/ci-puppet':}
-    people::samjsharpe::repos::gh  {'alphagov/boxgrinder-appliances':}
-    people::samjsharpe::repos::gh  {'alphagov/fabric-scripts':}
-    people::samjsharpe::repos::gh  {'alphagov/machine-bootstrap':}
-    people::samjsharpe::repos::gh  {'alphagov/service-domain-checker':}
-    people::samjsharpe::repos::gh  {'alphagov/ubuntu-1204-dev':}
-    people::samjsharpe::repos::gh  {'alphagov/vcloud-tools':}
+    people::samjsharpe::repos::gh  {[
+        'alphagov/ci-puppet',
+        'alphagov/boxgrinder-appliances',
+        'alphagov/fabric-scripts',
+        'alphagov/machine-bootstrap',
+        'alphagov/service-domain-checker',
+        'alphagov/ubuntu-1204-dev',
+        'alphagov/vcloud-tools'
+        ]:}
 
-    people::samjsharpe::repos::gh  {'samjsharpe/fgaas':}
-    people::samjsharpe::repos::gh  {'samjsharpe/samjsharpe.github.com':}
-    people::samjsharpe::repos::gh  {'samjsharpe/vagrant-centos':}
-    people::samjsharpe::repos::gh  {'samjsharpe/vagrant-rackspace':}
-    people::samjsharpe::repos::gh  {'samjsharpe/vagrant-ubuntu':}
+
+    # Personal repos
+    people::samjsharpe::repos::gh  {[
+        'samjsharpe/fgaas',
+        'samjsharpe/samjsharpe.github.com',
+        'samjsharpe/vagrant-centos',
+        'samjsharpe/vagrant-rackspace',
+        'samjsharpe/vagrant-ubuntu'
+        ]:}
 
     people::samjsharpe::repos::gh  {'samjsharpe/dotfiles': notify => Exec['samjsharpe-link-my-dotfiles'] }
     exec {'samjsharpe-link-my-dotfiles':
