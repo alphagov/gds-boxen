5423e5131473b8c5bb21d65bde67e01091b8e932
Sam J Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2013-12-14 15:02:37 +0000

    [samjsharpe] add gpg-agent

--- modules/people/manifests/samjsharpe/packages.pp
+++ modules/people/manifests/samjsharpe/packages.pp
@@ -50,6 +50,7 @@ PROMPT=\'$(virtualenv_prompt_info)%{$reset_color%}[%{$fg[cyan]%}%2d$(git_prompt_
       'ctags',
       'dtrx',
       'fping',
+      'gpg-agent',
       'heroku-toolbelt',
       'htop-osx',
       'nmap',
