6c97f2b503cb222617e3af3108558bcb7c8a6c2f
Sam J Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2014-01-07 21:18:24 +0000

    Update to the latest modules
    - Updates modules to latest upstream versions
    - Updates puppet-ruby module to 7.1.3
    
        This has consequences. I needed to update manifest to use the
        ruby::version defined types rather than simple includes. By
        including ripieenar/puppet-module-data we get the simply ruby
        version aliases as they are now defined in hiera in the data/
        directory of the ruby module.

--- manifests/site.pp
+++ manifests/site.pp
@@ -95,10 +95,10 @@ node default {
   }
 
   # default ruby versions
-  include ruby::1_8_7
-  include ruby::1_9_2
-  include ruby::1_9_3
-  include ruby::2_0_0
+  ruby::version {'1.8.7': }
+  ruby::version {'1.9.2': }
+  ruby::version {'1.9.3': }
+  ruby::version {'2.0.0': }
 
   # common, useful packages
   package {
