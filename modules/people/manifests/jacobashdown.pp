ba67f3f2cac6e159ef06792ed06ac30b20069835
Jacob Ashdown <jcbashdown@gmail.com>
2014-01-10 11:31:43 +0000

    add a starter manifest

---
+++ modules/people/manifests/jacobashdown.pp
@@ -0,0 +1,36 @@
+class people::jacobashdown  {
+  include chrome
+  include firefox
+  include spotify
+  include vim
+  include caffeine
+  include alfred
+  include vlc
+
+  # Dev VM
+  include vagrant
+  include virtualbox
+  vagrant::plugin { 'vagrant-dns': }
+  include projects::puppet
+
+  include teams::performance-platform
+
+  include projects::deployment
+  include projects::vagrant-govuk
+  include projects::vcloud-templates
+
+  class { 'gds-ssh-config': }
+  ssh_config::fragment{'performance-platform':
+    content => template('teams/performance-platform/ssh-config'),
+  }
+
+  package {
+    [
+      'wget',
+      'tmux',
+      'node',
+      'python'
+    ]:
+    ensure => present,
+  }
+}
