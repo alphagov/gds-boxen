7cc5ddc1e3ac547b95e8949dffc74d5295b55aed
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2014-01-14 20:22:15 +0000

    [alexmuller] Update personal boxen
    I've learnt so much from you folk over the last 8 months <3

--- modules/people/manifests/alexmuller.pp
+++ modules/people/manifests/alexmuller.pp
@@ -1,6 +1,8 @@
 class people::alexmuller {
-  include alfred
+  # Mac apps
+  include alfred::two
   include chrome
+  include dropbox
   include firefox
   include spotify
   include sublime_text_2
@@ -11,23 +13,31 @@ class people::alexmuller {
   vagrant::plugin { 'vagrant-dns': }
   include projects::puppet
 
+  # PP
   include teams::performance-platform
 
+  # Infrastructure and ops
+  include gds_vpn_profiles
   include projects::deployment
   include projects::vagrant-govuk
   include projects::vcloud-templates
 
+  # SSH
   class { 'gds-ssh-config': }
-  ssh_config::fragment{'performance-platform':
+  ssh_config::fragment { 'performance-platform':
     content => template('teams/performance-platform/ssh-config'),
   }
-  ssh_config::fragment{'alexmuller':
+  ssh_config::fragment { 'alexmuller':
     source => 'puppet:///modules/people/alexmuller/ssh-config',
   }
 
-  #include osx::dock::2d
-  #include osx::dock::autohide
+  # Git
+  git::config::global { 'color.ui': value => 'true' }
+  git::config::global { 'user.name': value => 'Alex Muller' }
+  git::config::global { 'user.email': value => 'alex.muller@digital.cabinet-office.gov.uk' }
 
+  # Homebrew packages
+  # Remember to read the autojump caveats: brew info autojump
   package {
     [
       'autojump',
@@ -36,4 +46,17 @@ class people::alexmuller {
     ]:
     ensure => present,
   }
+
+  $home_directory = '/Users/alexmuller'
+
+  # Vim config
+  file { "${home_directory}/.vimrc":
+    source => 'puppet:///modules/people/alexmuller/vimrc',
+  }
+
+  # Sublime Text 2 configuration
+  file { "${home_directory}/Library/Application Support/Sublime Text 2/Packages/User/Preferences.sublime-settings":
+    source => 'puppet:///modules/people/alexmuller/User-Preferences.sublime-settings',
+  }
+
 }
