20cab4d5ec2c81e9971dd645e7b8f0fb151db6f3
Isabell Long <isabell.long@digital.cabinet-office.gov.uk>
2014-02-10 17:20:51 +0000

    [issyl0] Add personal manifest

---
+++ modules/people/manifests/issyl0.pp
@@ -0,0 +1,11 @@
+class people::issyl0 {
+  include gds-development
+  include gds-resolver
+  include gds-ssh-config
+  include skype
+  include sublime_text_2
+  include wget
+  include skype
+  include iterm2::stable
+  include dropbox
+}
