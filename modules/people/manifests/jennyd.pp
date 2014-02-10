fef91f44848ad9677f75706076839efe423b5392
Jenny Duckett <jenny.duckett@digital.cabinet-office.gov.uk>
2014-02-10 09:18:22 +0000

    [jennyd] Update personal manifest
    Mostly projects and osx defaults

--- modules/people/manifests/jennyd.pp
+++ modules/people/manifests/jennyd.pp
@@ -3,6 +3,9 @@ class people::jennyd {
   include dropbox
   include encfs
   include gds-development
+  include gds-resolver
+  include gds-ssh-config
+  include gds_vpn_profiles
   include gnupg
   include iterm2::stable
   include skype
@@ -13,38 +16,55 @@ class people::jennyd {
   vagrant::plugin { 'vagrant-cachier': }
   vagrant::plugin { 'vagrant-zz-multiprovider-snap': }
 
+  include projects::alphagov-deployment
+  include projects::ci-deployment
+  include projects::ci-puppet
+  include projects::deployment::creds
   include projects::development
   include projects::fabric-scripts
+  include projects::frontend
+  include projects::gds-api-adapters
+  include projects::gds-sso
+  include projects::govuk_content_api
+  include projects::govuk_mirror
+  include projects::imminence
   include projects::opsmanual
+  include projects::private-utils
   include projects::puppet
   include projects::redirector
+  include projects::release
+  include projects::rummager
+  include projects::signonotron2
+  include projects::smokey
+  include projects::static
   include projects::vagrant-govuk
+  include projects::whitehall
 
   Boxen::Osx_defaults {
     user => $::luser,
   }
 
-  boxen::osx_defaults { 'Do not create .DS_Store':
-    key    => 'DSDontWriteNetworkStores',
-    domain => 'com.apple.dashboard',
-    value  => 'true',
-  }
-
-  include osx::dock::autohide
   include osx::disable_app_quarantine
+  include osx::dock::autohide
+  include osx::finder::show_hidden_files
+  include osx::finder::unhide_library
+  include osx::global::disable_autocorrect
+  include osx::global::enable_keyboard_control_access
+  include osx::global::key_repeat_delay
+  include osx::global::key_repeat_rate
+  include osx::no_network_dsstores
 
-  class { 'osx::global::key_repeat_delay':
-    delay => 200
+  class { 'osx::global::natural_mouse_scrolling':
+    enabled => false
   }
 
-  class { 'osx::global::key_repeat_rate':
-    rate => 400
+  class { 'osx::dock::position':
+    position => 'left'
   }
 
   package {
     [
       'python',
-      'pwgen',
       'tmux',
     ]:
     ensure => present,
@@ -59,6 +79,10 @@ class people::jennyd {
     provider => pip,
   }
 
+  ssh_config::fragment { 'jennyd':
+    source => 'puppet:///modules/people/jennyd/ssh-config',
+  }
+
   $home     = "/Users/${::luser}"
   $dotfiles = "${home}/dotfiles"
 
@@ -69,8 +93,8 @@ class people::jennyd {
 
   exec { 'join-the-dots':
     cwd         => $dotfiles,
-    command     => "./join-the-dots.sh",
-    refreshonly => true,
+    command     => "bash join-the-dots.sh",
+    logoutput   => true,
   }
 
 }
