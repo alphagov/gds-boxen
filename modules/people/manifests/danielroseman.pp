68fc527a997a10eaf7eb9a909a81c2c367e00252
Daniel Roseman <daniel@roseman.org.uk>
2014-02-13 14:31:27 +0000

    [danielroseman] add personal manifest.

---
+++ modules/people/manifests/danielroseman.pp
@@ -0,0 +1,55 @@
+class people::danielroseman {
+  include alfred
+  include chrome
+  include dropbox
+  include gds-development
+  include gds_vpn_profiles
+  include git
+  include hub
+  include iterm2::stable
+  include macvim
+  include vagrant
+  include virtualbox
+  include wget
+  include xquartz
+
+  $projects = "${home}/projects"
+
+  $home = "/Users/${::luser}"
+  $home_projects = "${home}/Projects"
+
+  file { $home_projects:
+    ensure => directory,
+  }
+
+  repository { "${home}/.vim/bundle/vundle":
+    source => 'gmarik/vundle',
+  }
+
+  $dotfiles = "${home_projects}/dotfiles"
+
+  repository { "${dotfiles}":
+    source => 'danielroseman/dotfiles',
+    require => File[$home_projects],
+    notify  => Exec['link-dotfiles'],
+  }
+
+  exec { 'link-dotfiles':
+    command     => "cd ${dotfiles} && sh dotfiles.sh",
+    refreshonly => true,
+  }
+
+  package {
+    [
+      'tmux',
+      'ctags',
+    ]:
+    ensure => present,
+  }
+
+  include python::2_7_6
+  python::package { 'virtualenv for python 2.7.6': package => 'virtualenv', python_version => '2.7.6' }
+
+}
+
+
