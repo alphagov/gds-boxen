e1da5a9a8f2377413f6a293a1011dfc0bdcedd2e
James Abley <james.abley@gmail.com>
2014-03-17 11:12:16 +0000

    [jabley] show hidden files in Finder
    I like being able to delete .fog properly, after putting credentials
    in plain text in it.

--- modules/people/manifests/jabley.pp
+++ modules/people/manifests/jabley.pp
@@ -195,6 +195,7 @@ class people::jabley {
   include osx::dock::2d
   include osx::finder::empty_trash_securely
   include osx::finder::show_all_on_desktop
+  include osx::finder::show_hidden_files
   include osx::finder::unhide_library
   include osx::global::expand_print_dialog
   include osx::global::expand_save_dialog
