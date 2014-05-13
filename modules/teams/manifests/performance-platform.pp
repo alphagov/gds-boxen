04412d6949daf50960b7dfa3fdcae50179135828
James Abley <james.abley@gmail.com>
2014-05-13 12:20:59 +0100

    Reflect repository name change
    libretto -> performanceplatform-documentation

--- modules/teams/manifests/performance-platform.pp
+++ modules/teams/manifests/performance-platform.pp
@@ -57,7 +57,7 @@ class teams::performance-platform {
   include projects::styleguides
 
   # Documentation
-  repo::alphagov { 'libretto': }
+  repo::alphagov { 'performanceplatform-documentation': }
   repo::alphagov { 'performance-platform': }
 
   # Office-y things
