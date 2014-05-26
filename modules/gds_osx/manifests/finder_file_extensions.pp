0638c1595ce2e30a5d56046493313cc0fa295814
Jorden Whitefield <jorden.whitefield@googlemail.com>
2014-05-26 23:52:58 +0100

    Corrected comment and exec typo.

--- modules/gds_osx/manifests/finder_file_extensions.pp
+++ modules/gds_osx/manifests/finder_file_extensions.pp
@@ -1,6 +1,6 @@
 # Shows file extensions and doesn't warn when the extension changes
 class gds_osx::finder_file_extensions {
-  exec { 'ql-text-selection':
+  exec { 'finder-file-extensions':
     command => 'sudo defaults write NSGlobalDomain AppleShowAllExtensions -bool true && defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false && killall SystemUIServer'
   }
 }
