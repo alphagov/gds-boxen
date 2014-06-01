8bc4fcb72582c1b9f343af76670464575a70e3e5
James Abley <james.abley@gmail.com>
2014-06-01 11:59:06 +0100

    Define experimental module

---
+++ modules/teams/manifests/performance-platform/experimental.pp
@@ -0,0 +1,9 @@
+# Boxen team definition for the Performance Platform
+# experiments.
+#
+# This is typically code containing sketches and ideas for
+# new features
+# include teams::performance-platform::experimental
+class teams::performance-platform::experimental {
+  repo::alphagov { 'performanceplatform-dashboard-admin': }
+}
