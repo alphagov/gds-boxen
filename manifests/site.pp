b225740fd361cac61a0abe0f4270e04a6b985cc5
James Abley <james.abley@gmail.com>
2014-06-01 12:26:41 +0100

    Remove ruby 1.9.2 because it's buggy
    People will need to `rbenv uninstall` various things too.

--- manifests/site.pp
+++ manifests/site.pp
@@ -69,7 +69,6 @@ node default {
 
   # default ruby versions
   ruby::version {'1.8.7': }
-  ruby::version {'1.9.2': }
   ruby::version {'1.9.3': }
   ruby::version {'2.0.0': }
   ruby::version {'2.1.0': }
