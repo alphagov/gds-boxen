fb9cad487d79992833dcc6bbcb323e19563cd869
Sam J Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2014-06-17 09:16:56 +0100

    [samjsharpe] update git

--- modules/people/manifests/samjsharpe/packages.pp
+++ modules/people/manifests/samjsharpe/packages.pp
@@ -45,7 +45,7 @@ PROMPT=\'$(virtualenv_prompt_info)%{$reset_color%}[%{$fg[cyan]%}%2d$(git_prompt_
 
   # Clobber boxen version of Git to use stock homebrew
   Package <| title == "boxen/brews/git" |> {
-    ensure => "1.9.2"
+    ensure => "2.0.0"
   }
 
   # These are all Homebrew packages
