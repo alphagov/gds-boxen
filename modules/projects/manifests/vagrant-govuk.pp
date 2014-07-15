f83d6033677f948eabe583f0b088ab2ad8f9a954
Matt Bostock <matt@mattbostock.com>
2014-07-15 20:48:25 +0100

    Remove vcloud-templates repo
    The 'vagrant-govuk' repo no longer uses it as of
    alphagov/vagrant-govuk@530ff06da4142f7739dce2d64fa6cbd74eaac7bd.
    
    We're now using 'govuk-provisioning' to provision GOV.UK VMs.
    
    /cc @samjsharpe @robyoung @alexmuller

--- modules/projects/manifests/vagrant-govuk.pp
+++ modules/projects/manifests/vagrant-govuk.pp
@@ -1,6 +1,6 @@
 # Pulls the https://github.com/alphagov/vagrant-govuk repository
 class projects::vagrant-govuk {
-  include projects::vcloud-templates
+  include projects::govuk-provisioning
   include projects::puppet
 
   repo::alphagov { 'vagrant-govuk': }
