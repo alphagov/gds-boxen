685a40ef7e4dfb2f519dd28054af7bdb2ac178ed
Matt Bostock <matt@mattbostock.com>
2014-07-15 20:44:48 +0100

    Remove boxes repo

--- modules/projects/manifests/boxes.pp
+++
@@ -1,4 +0,0 @@
-# Pulls the https://github.com/alphagov/boxes repository
-class projects::boxes {
-  repo::alphagov { 'boxes': }
-}
--- modules/teams/manifests/infrastructure.pp
+++ modules/teams/manifests/infrastructure.pp
@@ -1,6 +1,5 @@
 class teams::infrastructure ($manage_gitconfig = true) {
   include projects::alphagov-deployment
-  include projects::boxes
   include projects::boxgrinder-appliances
   include projects::boxmaker
   include projects::ci-puppet
