f94f07b859ad7dda6dae01bb3a22ea3655eaddcd
bob walker <bob.walker@digital.cabinet-office.gov.uk>
2014-09-23 09:26:07 +0100

    Add project definitions for Digital Market Place.

---
+++ modules/projects/manifests/digitalmarketplace-infra.pp
@@ -0,0 +1,4 @@
+# Pulls the https://github.gds/gds/digitalmarketplace-infra repository
+class projects::digitalmarketplace-infra {
+  repo::gds { 'digitalmarketplace-infra': }
+}
---
+++ modules/projects/manifests/digitalmarketplace-puppet.pp
@@ -0,0 +1,4 @@
+# Pulls the https://github.gds/gds/digitalmarketplace-puppet repository
+class projects::digitalmarketplace-puppet {
+  repo::gds { 'digitalmarketplace-puppet': }
+}
