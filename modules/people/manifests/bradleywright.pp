12666bb4dda3a6a19087f17ee2623ac8205c6f25
Bradley Wright <brad@intranation.com>
2014-10-22 21:38:22 +0100

    [bradleywright] Fix karabiner module
    This seems like a bug - the profile shipped with the app is called
    'Default' so I shouldn't need to define it.

--- modules/people/manifests/bradleywright.pp
+++ modules/people/manifests/bradleywright.pp
@@ -194,6 +194,8 @@ class people::bradleywright {
   include karabiner
   include karabiner::login_item
 
+  karabiner::profile { 'Default': }
+
   # Tap Ctrl_l for <esc>, hold for <ctrl>
   karabiner::remap { 'controlL2controlL_escape': }
   karabiner::set { 'parameter.keyoverlaidmodifier_timeout':
