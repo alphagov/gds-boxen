7485ca384a3a822e2487d5cf3acb893778317cdc
Matt Cottingham <matt.cottingham@digital.cabinet-office.gov.uk>
2014-11-06 11:48:26 +0000

    Update wireshark to 1.12.1
    The download for 1.10.7 no longer exists.

--- modules/wireshark/manifests/init.pp
+++ modules/wireshark/manifests/init.pp
@@ -1,4 +1,4 @@
-class wireshark($version='1.10.7') {
+class wireshark($version='1.12.1') {
   package { 'Wireshark':
     source   => "http://wiresharkdownloads.riverbed.com/wireshark/osx/Wireshark%20${version}%20Intel%2064.dmg",
     provider => 'pkgdmg',
