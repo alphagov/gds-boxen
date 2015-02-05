bc414dee432187fa550b738cc1436e3111308342
Dafydd Vaughan <dafydd.vaughan@digital.cabinet-office.gov.uk>
2015-02-05 16:13:49 +0000

    [daibach] Add various packages and settings

--- modules/people/manifests/daibach.pp
+++ modules/people/manifests/daibach.pp
@@ -2,6 +2,9 @@ class people::daibach {
 # System stuff
   include gds_osx::turn_off_dashboard
   include gds_osx::remove_spotlight
+  include gds_resolver
+  include gds_development
+  include gds_vpn_profiles
   include gds_virtualbox
   include openconnect
 
@@ -16,18 +19,31 @@ class people::daibach {
 
 # Apps I use
   include alfred
+  include atom
   include caffeine
   include chrome
   include dropbox
+  include evernote
+  include firefox
+  include googledrive
   include onepassword
+  include slack
   include spotify
   include sublime_text_2
   include textmate
+
+# OSX settings
+  include osx::no_network_dsstores
+  include osx::global::tap_to_click
+  include osx::dock::autohide
+  include osx::finder::empty_trash_securely
+  include osx::finder::show_all_filename_extensions
 
   include teams::mainstream
 
   package {
     [
+      'bash-completion',
       'mongodb',
       'rabbitmq',
     ]:
