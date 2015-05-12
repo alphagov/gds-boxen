6043d57666612a3fe67f960631f1f902325c897f
David Heath <david.heath@digital.cabinet-office.gov.uk>
2015-05-12 10:39:16 +0100

    [heathd] Add boxen config

---
+++ modules/people/manifests/heathd.pp
@@ -0,0 +1,70 @@
+class people::heathd {
+  include gds_development
+
+  include alfred
+  include chrome
+  include gitx
+  include mysql
+  include onepassword
+  include sublime_text_3
+  include slate
+  include things
+  include gds_development
+  include gds_osx::turn_off_dashboard
+  include gds_resolver
+  include gds_ssh_config
+  include gds_vpn_profiles
+
+  vagrant::plugin { 'vagrant-cachier': }
+  vagrant::plugin { 'vagrant-zz-multiprovider-snap': }
+
+  include projects::alphagov-deployment
+  include projects::ci-deployment
+  include projects::ci-puppet
+  include projects::deployment::creds
+  include projects::development
+  include projects::fabric-scripts
+  include projects::frontend
+  include projects::gds-api-adapters
+  include projects::gds-sso
+  include projects::govuk_content_api
+  include projects::govuk_mirror
+  include projects::opsmanual
+  include projects::private-utils
+  include projects::puppet
+  include projects::transition-config
+  include projects::rummager
+  include projects::signonotron2
+  include projects::smokey
+  include projects::static
+  include projects::vagrant-govuk
+  include projects::whitehall
+
+  include osx::disable_app_quarantine
+  include osx::dock::autohide
+  include osx::finder::show_hidden_files
+  include osx::finder::unhide_library
+  include osx::global::disable_autocorrect
+  include osx::global::enable_keyboard_control_access
+  include osx::global::key_repeat_delay
+  include osx::global::key_repeat_rate
+  include osx::no_network_dsstores
+
+
+  class { 'osx::global::natural_mouse_scrolling':
+    enabled => false
+  }
+
+
+  class { 'osx::dock::position':
+    position => 'left'
+  }
+
+  package {
+    [
+      'python',
+      'tmux',
+    ]:
+    ensure => present,
+  }
+}
