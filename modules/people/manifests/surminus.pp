d9346cba4cf08a1033b61ea43c03ad65c2cd937a
Paul Martin <Paul.martin@digital.cabinet-office.gov.uk>
2015-07-22 15:03:43 +0100

    [surminus] Added mission critical screensavers
    Added more screensavers as an option in brew
    because I don't like to leave my screen with an
    unexciting screensaver when I'm making coffee.

--- modules/people/manifests/surminus.pp
+++ modules/people/manifests/surminus.pp
@@ -1,4 +1,4 @@
-## Paul Martin boxen config :3 
+## Paul Martin boxen config :3
 class people::surminus {
   include alfred
   include caffeine
@@ -8,63 +8,69 @@ class people::surminus {
   include gds_vpn_profiles
   include git
   include gitx
-  include googledrive 
+  include googledrive
   include hub
   include iterm2::stable
   include keepassx
   include macvim
-#include openconnect # doesn't play ball with Yosemite
+  #include openconnect # doesn't play ball with Yosemite
   include screen
   include spotify
   include vagrant
   include virtualbox
   include zsh
   include ohmyzsh
+  include projects::deployment::creds
 
-  ## osx stuff 
+  ## osx stuff
 
-  osx::recovery_message { 'If this Mac is found, please call 07780 225 281': } 
+  osx::recovery_message { 'If this Mac is found, please call 07780 225 281': }
 
   $home    = "/Users/${::luser}"
   $mystuff = "${home}/surminus"
-  
+
   ## dotfiles
   repository { "${mystuff}/dotfiles":
     source  => 'surminus/dotfiles',
   }
 
   # junk scripts
-  repository { "${mystuff}/scripts": 
-    source => 'surminus/scripts', 
+  repository { "${mystuff}/scripts":
+    source => 'surminus/scripts',
   }
 
-  $vimrc = "${mystuff}/dotfiles/vimrc" 
+  $vimrc = "${mystuff}/dotfiles/vimrc"
   $zshrc = "${mystuff}/dotfiles/zshrc"
 
-  file { "${home}/.vimrc": 
-    ensure => 'link', 
+  file { "${home}/.vimrc":
+    ensure => 'link',
     target => "${mystuff}/dotfiles/vimrc",
   }
 
-  file { "${home}/.zshrc": 
-    ensure => 'link', 
+  file { "${home}/.zshrc":
+    ensure => 'link',
     target => "${mystuff}/dotfiles/zshrc",
   }
 
-  file { "${home}/Scripts/keepass-merge": 
-    ensure => 'link', 
+  file { "${home}/.ackrc":
+    ensure => 'link',
+    target => "${mystuff}/dotfiles/ackrc",
+  }
+
+  file { "${home}/Scripts/keepass-merge":
+    ensure => 'link',
     target => "${mystuff}/scripts/keepass-merge",
   }
 
-  file { [ "${mystuff}", "${home}/Scripts", "${home}/.vim", "${home}/.vim/bundle", "${home}/.vim/autoload"]: 
-    ensure => directory, 
+  file { [ "${mystuff}", "${home}/Scripts", "${home}/.vim", "${home}/.vim/bundle", "${home}/.vim/autoload"]:
+    ensure => directory,
   }
 
-  repository { "${home}/.vim/bundle/nerdtree": 
-    source => 'scrooloose/nerdtree', 
+  repository { "${home}/.vim/bundle/nerdtree":
+    source => 'scrooloose/nerdtree',
   }
 
-  exec { "curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim": 
+  exec { "curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim":
     unless => "ls ${home}/.vim/autoload/pathogen.vim"
   }
 
@@ -85,7 +91,6 @@ class people::surminus {
       'cmake',
       'coreutils',
       'docker',
-      'gnupg',
       'go',
       'htop-osx',
       'libxml2',
@@ -93,5 +98,14 @@ class people::surminus {
       'wget',
     ]:
     ensure => present,
+  }
+
+package {
+    [
+      'osxfuse',
+      'xscreensaver',
+    ]:
+    ensure   => present,
+    provider => 'brewcask',
   }
 }
