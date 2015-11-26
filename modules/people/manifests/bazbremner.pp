eb6980ab76f0ac35b41d9ffb70779d28274e8fb9
Barrie Bremner <barrie.bremner@digital.cabinet-office.gov.uk>
2015-11-26 11:04:51 +0000

    [bazbremner] fix nodejs version definition
    Changed as part of the El Cap updates.

--- modules/people/manifests/bazbremner.pp
+++ modules/people/manifests/bazbremner.pp
@@ -23,7 +23,7 @@ class people::bazbremner {
   include osx::finder::show_external_hard_drives_on_desktop
   include osx::finder::unhide_library
 
-  class { 'nodejs::global': version => 'v0.12.7' }
+  class { 'nodejs::global': version => '0.12.7' }
 
   class { 'osx::global::natural_mouse_scrolling':
     enabled => false
